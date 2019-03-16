e67f16c18ae0a3ee76064519add1a6ce36b5329c
Brett H <Horst.Brett@gmail.com>
2019-03-16 09:57:46 -0600

    Changing website

---
+++ precache-manifest.9e5cab10c03ae75370a0c69a0b97749a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a743134d51e0b73da074",
+    "url": "./static/js/main.d1d100fa.chunk.js"
+  },
+  {
+    "revision": "1124e0c9c5d75d7e1c57",
+    "url": "./static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "a743134d51e0b73da074",
+    "url": "./static/css/main.ba90e114.chunk.css"
+  },
+  {
+    "revision": "1124e0c9c5d75d7e1c57",
+    "url": "./static/css/2.435fa120.chunk.css"
+  },
+  {
+    "revision": "842bd7b2e6c0ce611993b544cf94926f",
+    "url": "./index.html"
+  }
+];
